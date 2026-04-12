d9af11ee207450c7b53b3762a66339d3e37eb657
Ashutosh Kumar <ashutoshkumar@Ashutoshs-MacBook-Air-4.local>
2026-04-13 04:10:37 +0530

    Deployment issues addressed

--- backend/utils/mailSender.js
+++ backend/utils/mailSender.js
@@ -3,16 +3,16 @@ import nodemailer from "nodemailer"
 const mailSender = async (email, title, body) => {
     try{
             let transporter = nodemailer.createTransport({
-                host:process.env.MAIL_HOST,
+                host:process.env.BREVO_SMTP_HOST,
                 auth:{
-                    user: process.env.MAIL_USER,
-                    pass: process.env.MAIL_PASS,
+                    user: process.env.BREVO_SMTP_USER,
+                    pass: process.env.BREVO_SMTP_PASS,
                 }
             })
 
 
             let info = await transporter.sendMail({
-                from: 'Edvora || Edvora - by Aryan Singh',
+                from: `${process.env.BREVO_FROM_NAME} <${process.env.BREVO_FROM_EMAIL}>`,
                 to:`${email}`,
                 subject: `${title}`,
                 html: `${body}`,
